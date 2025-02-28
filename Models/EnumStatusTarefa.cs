1178cc69f551896f7aa83360c4afe6909ec8c69a
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2025-02-28 10:40:57 -0300

    Update EnumStatusTarefa.cs

--- Models/EnumStatusTarefa.cs
+++ Models/EnumStatusTarefa.cs
@@ -1,5 +1,7 @@
 namespace TrilhaApiDesafio.Models
 {
+// deixa o código mais intuitivo, compreensivel, implementando valores que sejam efetivamente validos. Pra mais infos, é bom ler esse material: https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/builtin-types/enum 
+
     public enum EnumStatusTarefa
     {
         Pendente,
